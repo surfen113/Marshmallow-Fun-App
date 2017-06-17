c20fa802e095669d59ecd81494b426f7e1adaa5d
surfen113 <ritter.julian@googlemail.com>
2017-06-17 14:29:03 +0200

    Correct Implementation of the MapController, Implementation of Markers

--- frontend/src/components/view-map/view-map.component.js
+++ frontend/src/components/view-map/view-map.component.js
@@ -6,13 +6,18 @@
 
 import template from './view-map.template.html';
 import UserService from './../../services/user/user.service';
-
+import NgMap from 'ngmap';
 
 class ViewMapComponent {
     constructor() {
 
         this.template = template;
         this.controller= ViewMapController;
+
+    }
+
+    testFunction() {
+        alert("yaaaay");
     }
 
     static get name() {
@@ -21,7 +26,33 @@ class ViewMapComponent {
 }
 
 class ViewMapController {
+    constructor($state,NgMap,UserService){
+        this.$state = $state;
+        this.NgMap = NgMap;
+        this.UserService = UserService;
 
+        this.markerPoints = [
+            { "name": "Canberra", "latitude": 48.261, "longitude": 11.669 },
+            { "name": "Melbourne", "latitude": 48.262, "longitude": 11.669  },
+            { "name": "Sydney", "latitude": 48.263, "longitude": 11.669  }
+        ];
+
+        NgMap.getMap().then(function(map) {
+            //map.markers.set()
+
+            //map.markers.add
+            console.log(map.getCenter());
+            console.log('markers', map.markers);
+            console.log('shapes', map.shapes);
+        });
+
+    }
+
+
+
+    static get $inject() {
+        return ['$state', 'NgMap', UserService.name]
+    }
 }
 
 export default ViewMapComponent;
