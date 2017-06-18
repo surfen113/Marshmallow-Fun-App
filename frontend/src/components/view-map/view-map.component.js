c2890b9255bcf6ac5a73a0c5d81bee37bd63298b
surfen113 <ritter.julian@googlemail.com>
2017-06-18 20:14:37 +0200

    Implementation of Map Pop-Ups, etc.

--- frontend/src/components/view-map/view-map.component.js
+++ frontend/src/components/view-map/view-map.component.js
@@ -25,34 +25,50 @@ class ViewMapComponent {
 
 class ViewMapController {
     constructor($state,NgMap,UserService){
+        var vm = this;
         this.$state = $state;
         this.NgMap = NgMap;
         this.UserService = UserService;
 
         this.markerPoints = [
-            { "name": "Canberra", "latitude": 48.261, "longitude": 11.669 },
-            { "name": "Melbourne", "latitude": 48.262, "longitude": 11.669  },
-            { "name": "Sydney", "latitude": 48.263, "longitude": 11.669  },
-            { "name": "Another Activity", "latitude": 47.263, "longitude": 10.669  },
-            { "name": "Rowing", "latitude": 48.2761, "longitude": 11.669  },
-            { "name": "Swimming", "latitude": 48.263, "longitude": 11.639  },
-            { "name": "Biking", "latitude": 48.263, "longitude": 11.645  },
-            { "name": "Visiting Munich", "latitude": 48.161, "longitude": 11.669  },
-            { "name": "Chilling", "latitude": 48.2761, "longitude": 11.699  },
-            { "name": "Studying Maths", "latitude": 48.261, "longitude": 11.799  },
-            { "name": "Baseball", "latitude": 45.263, "longitude": 15.669  },
-            { "name": "Soccer", "latitude": 51.263, "longitude": 12.669  },
-            { "name": "Start Wars", "latitude": 53.263, "longitude": -57.669  }
+            {id:"1", "name": "Canberra", "latitude": 48.261, "longitude": 11.669 },
+            {id:"2", "name": "Melbourne", "latitude": 48.262, "longitude": 11.669  },
+            {id:"3", "name": "Sydney", "latitude": 48.263, "longitude": 11.669  },
+            {id:"4", "name": "Another Activity", "latitude": 47.263, "longitude": 10.669  },
+            {id:"5", "name": "Rowing", "latitude": 48.2761, "longitude": 11.669  },
+            {id:"6", "name": "Swimming", "latitude": 48.263, "longitude": 11.639  },
+            {id:"7", "name": "Biking", "latitude": 48.263, "longitude": 11.645  },
+            {id:"8", "name": "Visiting Munich", "latitude": 48.161, "longitude": 11.669  },
+            {id:"9", "name": "Chilling", "latitude": 48.2761, "longitude": 11.699  },
+            {id:"10", "name": "Studying Maths", "latitude": 48.261, "longitude": 11.799  },
+            {id:"11", "name": "Baseball", "latitude": 45.263, "longitude": 15.669  },
+            {id:"12", "name": "Soccer", "latitude": 51.263, "longitude": 12.669  },
+            {id:"13", "name": "Start Wars", "latitude": 53.263, "longitude": -57.669  }
         ];
+        vm.marker = this.markerPoints[3];
 
         NgMap.getMap().then(function(map) {
             //map.markers.set()
+            vm.map = map;
 
             //map.markers.add
             console.log(map.getCenter());
             console.log('markers', map.markers);
             console.log('shapes', map.shapes);
         });
+
+        this.showStore = function(e, marker2) {
+console.log(marker2);
+            console.log(marker2.id);
+
+vm.marker = marker2;
+console.log(vm.marker);
+           // vm.store = this.markerPoints[1];
+            console.log("id: " + vm.marker.id);
+            vm.map.showInfoWindow('bar', vm.marker.id);
+        };
+
+
 
     }
 
