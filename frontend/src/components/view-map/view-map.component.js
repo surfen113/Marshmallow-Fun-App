c5dde438a7e6dba4cbe3ea5d11ce28b7aab56a1d
Samy <samy@cqse.eu>
2017-07-16 12:58:53 +0200

    minor formatting changes as well as expanded search function

--- frontend/src/components/view-map/view-map.component.js
+++ frontend/src/components/view-map/view-map.component.js
@@ -36,7 +36,7 @@ class ViewMapController {
         this.NgMap = NgMap;
         this.UserService = UserService;
         this.ActivitiesService = ActivitiesService;
-        this.activities =        this.ActivitiesService.list();
+        this.activities = this.ActivitiesService.list();
         this.FollowsService = FollowsService;
         this.JoinsService = JoinsService;
 
@@ -44,14 +44,14 @@ class ViewMapController {
         var newLongitude = null;
 
         this.ActivitiesService.list().then(data => {
-        //    console.log(data);
-        //console.log(JSON.stringify(data));
-        this.settings = data;
-    });
+            //    console.log(data);
+            //console.log(JSON.stringify(data));
+            this.settings = data;
+        });
 
 
-        var filters = {social:false, sports:false, party:false, music:false, culture:false}
-        var map_filter = function(id) {
+        var filters = {social: false, sports: false, party: false, music: false, culture: false}
+        var map_filter = function (id) {
             if (filters[id]) {
                 filters[id] = false;
             }
@@ -61,58 +61,79 @@ class ViewMapController {
         }
 
 
-        $( document ).ready(function() {
-                $('input[name=filter]').change(function (e) {
-                    map_filter(this.id);
-                    filter_markers(vm.activities);
-                });
+        $(document).ready(function () {
+            $('input[name=filter]').change(function (e) {
+                map_filter(this.id);
+                filter_markers(vm.activities);
+            });
+            $('input[id=searchbar]').change(function (e) {
+                console.log("da5alna el searchbar ya bro");
+                var query = document.getElementById('searchbar').value;
+                console.log(query);
+                search_filter(query, vm.activities);
+            });
         });
 
+        var search_filter = function (query, activities) {
+            var markers = filter_markers(activities);
+            for (var i in markers) {
+                console.log(i);
+                if (i.title !== null && i.title.test(query) ){
+                    i.setMap(vm.map)
+                }
+                if (i.title !== null && !i.title.test(query) ){
+                    i.setMap(null);
+                }
+            }
+            google.maps.event.trigger(vm.map, 'resize');
+        }
 
-        var create_markers = function(activities) {
+        var create_markers = function (activities) {
             var markers = []
-            for(var i=0; i<activities.length; i++) {
-                var marker = new google.maps.Marker({position: {lat: activities[i].latitude, lng: activities[i].longitude},
+            for (var i = 0; i < activities.length; i++) {
+                var marker = new google.maps.Marker({
+                    position: {lat: activities[i].latitude, lng: activities[i].longitude},
                     setMap: null,
-                    title: activities[i].title})
+                    title: activities[i].title
+                })
                 markers.push(marker);
             }
             return markers;
         }
 
-        var filter_markers = function(activities) {
-            var markers=[];
-            var markers2=[];
+        var filter_markers = function (activities) {
+            var markers = [];
+            var markers2 = [];
             for (var i = 0; i < activities.length; i++) {
                 var acceptable = false;
                 for (var opt in filters) {
-                    switch(opt) {
+                    switch (opt) {
                         case "sports":
-                            if (filters[opt]){
+                            if (filters[opt]) {
                                 acceptable = activities[i].sports;
-                               markers.push(activities[i])
+                                markers.push(activities[i])
                             }
                             break;
                         case "culture":
-                            if (filters[opt]){
+                            if (filters[opt]) {
                                 acceptable = activities[i].culture;
                                 markers.push(activities[i]);
                             }
                             break;
                         case "party":
-                            if (filters[opt]){
+                            if (filters[opt]) {
                                 acceptable = activities[i].party;
                                 markers.push(activities[i]);
                             }
                             break;
                         case "music":
-                            if (filters[opt]){
+                            if (filters[opt]) {
                                 acceptable = activities[i].music;
                                 markers.push(activities[i]);
                             }
                             break;
                         case "social":
-                            if (filters[opt]){
+                            if (filters[opt]) {
                                 acceptable = activities[i].social;
                                 markers.push(activities[i]);
                             }
@@ -120,32 +141,34 @@ class ViewMapController {
                         default:
 
                     }
-                    if(acceptable){
+                    if (acceptable) {
                         break;
                     }
 
-                    if(!acceptable && opt == "culture"){
-                      markers2.push(activities[i]);
+                    if (!acceptable && opt == "culture") {
+                        markers2.push(activities[i]);
                     }
                 }
+                return markers
             }
             var toadd = create_markers(markers);
             var toremove = create_markers(markers2);
-            for (var i = 0; i< toadd.length; i++) {
-               toadd[i].setVisible(true);
+            for (var i = 0; i < toadd.length; i++) {
+                toadd[i].setMap(vm.map);
             }
-            for (var i = 0; i< toremove.length; i++) {
-                toremove[i].setVisible(false);
+            for (var i = 0; i < toremove.length; i++) {
+                toremove[i].setMap(null);
             }
+            google.maps.event.trigger(vm.map, 'resize');
         }
 
         NgMap.getMap().then(function (map) {
             vm.map = map;
 
-            map.addListener('click', function(e) {
+            map.addListener('click', function (e) {
                 newLatitude = e.latLng.lat();
                 newLongitude = e.latLng.lng();
-                if(UserService.isAuthenticated()) {
+                if (UserService.isAuthenticated()) {
                     var marker = new google.maps.Marker({
                         position: e.latLng,
                         map: map,
@@ -158,17 +181,17 @@ class ViewMapController {
         });
 
 
-        this.getLatitude = function() {
+        this.getLatitude = function () {
             return newLatitude;
         }
 
-        this.getLongitude = function() {
+        this.getLongitude = function () {
             return newLongitude;
         }
 
         this.showStore = function (e, marker2) {
             vm.marker = marker2;
-            if(UserService.isAuthenticated()) {
+            if (UserService.isAuthenticated()) {
                 vm.map.showInfoWindow('normalActivity', vm.marker._id);
             }
         }
@@ -181,7 +204,7 @@ class ViewMapController {
 
 
     static get $inject() {
-        return ['$state','$scope', 'NgMap', ActivitiesService.name, UserService.name, FollowsService.name, JoinsService.name];
+        return ['$state', '$scope', 'NgMap', ActivitiesService.name, UserService.name, FollowsService.name, JoinsService.name];
     }
 
 
@@ -195,11 +218,11 @@ class ViewMapController {
         this.activity['longitude'] = longitude;
         var date = this.activity['datetime'];
 
-        if(date == null){
+        if (date == null) {
             this.activity['date'] = "";
-        }else{
+        } else {
             date = date.toLocaleString();
-            date = date.substring(0,10);
+            date = date.substring(0, 10);
             this.activity['date'] = date;
         }
 
@@ -209,23 +232,23 @@ class ViewMapController {
 //        data.birthday = new Date(data.birthday);
         this.ActivitiesService.create(this.activity).then(data => {
             let _id = data[_id];
-        this.$state.go('myActivities',{activityId:_id});
-    });
+            this.$state.go('myActivities', {activityId: _id});
+        });
 
     }
 
 
-    cancel(){
+    cancel() {
         this.$state.reload();
     }
 
-    isAuthenticated(){
+    isAuthenticated() {
         return this.UserService.isAuthenticated();
     }
 
     isOwnActivity(userID) {
         let user = this.UserService.getCurrentUser();
-        if(userID == user['_id']) {
+        if (userID == user['_id']) {
             return true;
         }
         else {
@@ -235,13 +258,13 @@ class ViewMapController {
 
     follow() {
         if (this.UserService.isAuthenticated()) {
-        //let follower = this.UserService.getCurrentUser();
+            //let follower = this.UserService.getCurrentUser();
 
-           this.FollowsService.create(this.marker.user, this.UserService.getCurrentUser()['_id']).then(data =>{
-                this.$state.go('userProfile', { userId:this.marker.user });
+            this.FollowsService.create(this.marker.user, this.UserService.getCurrentUser()['_id']).then(data => {
+                this.$state.go('userProfile', {userId: this.marker.user});
             });
         } else {
-            this.$state.go('login',{});
+            this.$state.go('login', {});
         }
     }
 
@@ -249,23 +272,22 @@ class ViewMapController {
         let user = this.UserService.getCurrentUser();
 
 
-
         if (this.UserService.isAuthenticated()) {
             //let follower = this.UserService.getCurrentUser();
-            this.JoinsService.create( this.UserService.getCurrentUser()['_id'], id);
+            this.JoinsService.create(this.UserService.getCurrentUser()['_id'], id);
         } else {
-            this.$state.go('login',{});
+            this.$state.go('login', {});
         }
 
     }
 
-    unjoin(id){
+    unjoin(id) {
 
     }
 
     details(activity) {
         let _id = activity['_id'];
-        this.$state.go('activity', {activityId:_id});
+        this.$state.go('activity', {activityId: _id});
     }
 
 }
